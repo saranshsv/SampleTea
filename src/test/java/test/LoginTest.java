45d7bbd1fc9748569e461ad98512829f311fd548
Saransh <saranshsv@gmail.com>
2020-05-18 21:10:31 +0530

    Initial Commit

---
+++ src/test/java/test/LoginTest.java
@@ -0,0 +1,38 @@
+package test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+
+	
+	public static WebDriver driver;
+	
+	@BeforeSuite
+	public void setUp() {
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+	}
+	
+	@Test
+	public void doLogin() {
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("http://gmail.com");
+		driver.findElement(By.id("identifierId")).sendKeys("saranshsv@gmail.com");
+		driver.findElement(By.xpath("//*[@id='identifierNext']")).click();
+		//driver.findElement(By.name("password")).sendKeys("CeLciusGM93");
+	}
+	
+	@AfterSuite
+	public void tearDown() {
+		driver.quit();
+	}
+}
